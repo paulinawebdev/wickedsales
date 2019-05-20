db22ac797df426e254fd16411e35dac61b6bf4eb
paulinawebdev <paulinawebdev@gmail.com>
2019-05-20 11:18:02 -0700

    app displaying header

---
+++ client/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+      <header>
+        <div className="logo"><h1>Wicked Sales</h1></div>
+      </header>
+    );
+  }
+}
