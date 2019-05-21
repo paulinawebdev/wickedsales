9e40aab1087198ae4782786e8b7288db7b3e6dcc
paulinawebdev <paulinawebdev@gmail.com>
2019-05-21 15:46:42 -0700

    be 01 create product endpoint

---
+++ server/public/api/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+    function error_handler($error) {
+        $output = ["success" => false, "error" => $error->getMessage()];
+        $json_output = json_encode($output);
+        print $json_output;
+    }
+
+?>
