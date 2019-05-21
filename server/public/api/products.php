f9fc0a7ccf171fa772021bd131762fc699e7f86a
paulinawebdev <paulinawebdev@gmail.com>
2019-05-21 16:56:11 -0700

    be02 create connection mysql

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -2,9 +2,13 @@
 
   require_once './functions.php';
 
+  require_once './db_connection.php';
+
   set_exception_handler("error_handler");
 
-  dostuff();
+  if(!$conn){
+    throw new Exception('there is an error' . mysqli_connect_error());
+  }
 
   $output = file_get_contents('./dummy-products-list.json');
   print $output;
