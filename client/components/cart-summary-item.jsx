3d757816be8d31513f6350c7fdd942186ddfba99
paulinawebdev <paulinawebdev@gmail.com>
2019-05-22 10:38:23 -0700

    view cart summary front end

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -9,7 +9,7 @@ export default class CartSummaryItem extends React.Component {
     return (
       <div className="cart-item">
         <div className="cart-item-img"><img src={cartItem.image} /></div>
-        <div className="cart-item-desc">
+        <div className="cart-item-content">
           <div className="cart-item-name">{cartItem.name}</div>
           <div className="cart-item-price">${(cartItem.price / 100).toFixed(2)}</div>
           <div className="cart-item-desc">{cartItem.shortDescription}</div>
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -8,12 +8,20 @@ export default class CartSummary extends React.Component {
     const cart = this.props.cartSummary;
     let cartItems = cart.map(cartItem => <CartSummaryItem key={cartItem.id} item={cartItem} />);
 
+    let cartPrices = cart.map(cartPrice => cartPrice.price);
+
+    function getSum(total, num) {
+      return total + num;
+    }
+
+    let cartTotal = cartPrices.reduce(getSum, 0);
+
     return (
       <div className="cart-summary pg-width">
         <div className="back-btn" onClick={() => this.props.setViewCallback('catalog', {})}><i className="fas fa-chevron-left"></i> Back to Catalog</div>
         <h1>My Cart</h1>
         {cartItems.length ? cartItems : 'No Cart Items'}
-        <div className="total-price">222</div>
+        <div className="cart-total-price">Item Total: {(cartTotal / 100).toFixed(2)}</div>
       </div>
     );
   }
