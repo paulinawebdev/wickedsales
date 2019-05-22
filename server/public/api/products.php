66bf80dc949228ade59c11515fa8ec8b879c6eb5
paulinawebdev <paulinawebdev@gmail.com>
2019-05-21 20:05:18 -0700

    be04 connect and fetch

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -6,14 +6,40 @@
 
   set_exception_handler("error_handler");
 
+  startup();
+
   if(!$conn){
     throw new Exception('there is an error' . mysqli_connect_error());
   }
 
-  $output = file_get_contents('./dummy-products-list.json');
-  print $output;
+  $query = "SELECT * FROM productList";
 
+  $res = $conn->query($query);
+  if ($conn->error) {
+      try {    
+          throw new Exception("MySQL error $conn->error <br> Query:<br> $query");    
+      } catch(Exception $e ) {
+          echo "error: ". $e->getMessage();
+      }
+  }
   
+  if ($result = mysqli_query($conn, $query)) {
+      $numRows = mysqli_num_rows($result);
+  }
+
+  if ($numRows === 0) {
+    print "no data available";
+    exit();
+  }
+
+  $output = [];
+
+  while ($row = mysqli_fetch_assoc($result)) {  
+      array_push($output, $row);
+  }
+
+  $json_output = json_encode($output);
+    print $json_output;
 
 // header('Content-Type: application/json');
 
